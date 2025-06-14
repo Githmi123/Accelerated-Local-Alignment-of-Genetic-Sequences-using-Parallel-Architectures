a6ba06a317af79adf2ff51b12bff11af90f07df7
githmi <githmisundarasekara@gmail.com>
2025-06-14 20:12:35 +0530

    refactor: change name of the sequences file

--- data/generate_sequences.py
+++ data/generate_sequences.py
@@ -3,7 +3,7 @@ import random
 def generate_sequence(length):
     return ''.join(random.choices('ACGT', k=length)) # random.choices('ACGT', k=length) will output a list like ['A', 'G', 'C', 'T', 'T', 'A', 'C', 'G', 'G', 'T']
 
-with open("DNAsequences.txt", "w") as file:
+with open("DNASequences.txt", "w") as file:
     for i in range(1000):
         seq1 = generate_sequence(200)
         seq2 = generate_sequence(200)
