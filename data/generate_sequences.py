5f07f53e02c6840a52001aab65b595a7bf466d1d
githmi <githmisundarasekara@gmail.com>
2025-06-14 20:07:13 +0530

    feat: add a sequence generator

---
+++ data/generate_sequences.py
@@ -0,0 +1,10 @@
+import random
+
+def generate_sequence(length):
+    return ''.join(random.choices('ACGT', k=length)) # random.choices('ACGT', k=length) will output a list like ['A', 'G', 'C', 'T', 'T', 'A', 'C', 'G', 'G', 'T']
+
+with open("DNAsequences.txt", "w") as file:
+    for i in range(1000):
+        seq1 = generate_sequence(200)
+        seq2 = generate_sequence(200)
+        file.write(f"{seq1},{seq2}\n")
