88e7bec5dc375842a872f92723f49944e55feff8
githmi <githmisundarasekara@gmail.com>
2025-06-15 09:04:33 +0530

    feat: add calculation of time difference

--- serial_code/serial_code.c
+++ serial_code/serial_code.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <errno.h>
 #include "../constants.h"
+#include <sys/time.h>
 
 char seq1_list[MAX_PAIRS][MAX_SEQ_LENGTH];
 char seq2_list[MAX_PAIRS][MAX_SEQ_LENGTH];
@@ -30,4 +31,20 @@ int main()
 {
     int n = load_sequences("../data/DNASequences.txt");
     printf("Loaded %d pairs of sequences.\n", n);
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    for (int i = 0; i < n; i++)
+    {
+        // Process
+    }
+
+    gettimeofday(&end, NULL);
+
+    printf("Completed alignment of %d sequence pairs.\n", n);
+    printf("Time taken in seconds: %ld seconds\n", end.tv_sec - start.tv_sec);
+    printf("Time taken in microseconds: %ld microseconds\n", end.tv_usec - start.tv_usec);
+    printf("Total time taken: %0.7f seconds\n", 
+           (float)((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000));
 }
