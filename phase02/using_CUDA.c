73a957142359ea613fa2bef1c51c64e2f02e46fb
githmi <githmisundarasekara@gmail.com>
2025-06-17 10:06:17 +0530

    feat: add serial code

--- phase02/using_CUDA.c
+++ phase02/using_CUDA.c
@@ -10,8 +10,7 @@ char seq1_list[MAX_PAIRS][MAX_SEQ_LENGTH];
 char seq2_list[MAX_PAIRS][MAX_SEQ_LENGTH];
 int score_matrix[MAX_PAIRS];
 
-//TODO: Make this function common to all implementations
-int load_sequences(const char *filename) 
+int load_sequences(const char *filename)
 {
     FILE *file = fopen(filename, "r");
     if ( file == NULL ) {
